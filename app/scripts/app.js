6d3ff75c94c96d5fe483c8da9addbfbba3f47ad6
Fabrice PESTRE <dev.fabrice@yahoo.fr>
2015-01-29 09:58:08 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name openclassroomsApp
+ * @description
+ * # openclassroomsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('openclassroomsApp', []);
